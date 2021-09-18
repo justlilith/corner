9f45ad96ed2e661455478d8da1bae2242dfbf5b7
justlilith <hello@justlilith.com>
2021-09-18 03:03:17 -0500

    add endpoint -L6

---
+++ src/routes/journal/[slug].json.ts
@@ -0,0 +1,18 @@
+import { default as fs } from 'fs'
+
+export async function get({ params }){
+  const { slug } = params
+  const article = JSON.parse(fs.readFileSync(`src/routes/journal/posts/${slug}.json`,{encoding: 'utf8'}))
+  // let article
+  // await fs.readFile(`src/routes/journal/posts/${slug}.json`, (err, data) => {
+    // article = {"test":"test"}
+    // article = data
+  // })
+
+  
+  return {
+    body: {
+      article
+    }
+  }
+}
